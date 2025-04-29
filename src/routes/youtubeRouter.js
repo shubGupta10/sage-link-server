6c212bf82cfd23ec84dd98caf20821293e9edc5b
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2025-04-29 18:08:12 +0530

    Add authentication middleware to YouTube transcript and chat routes

--- src/routes/youtubeRouter.js
+++ src/routes/youtubeRouter.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { chatWithLink, getYoutubeVideoURL } from '../controllers/youtubeRouter.js';
+import { authenticate } from '../middlewares/authMiddleware.js';
 
 const youtubeRouter = express.Router();
 
-youtubeRouter.post("/get-transcript", getYoutubeVideoURL);
-youtubeRouter.post("/chat-with-link", chatWithLink);
+youtubeRouter.post("/get-transcript", authenticate ,getYoutubeVideoURL);
+youtubeRouter.post("/chat-with-link", authenticate, chatWithLink);
 
 export default youtubeRouter;
