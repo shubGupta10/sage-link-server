96d33002bd4122c7825f4c4503e20263f8f5a8de
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2025-04-30 11:02:37 +0530

    Simplify CORS configuration in server.js

--- src/server.js
+++ src/server.js
@@ -9,11 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 app.use(express.json());
-app.use(cors({
-    origin: process.env.FRONTEND_URL || "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-}));
+app.use(cors());
 
 app.use("/api/users", userRouter);
 app.use("/api/youtube", youtubeRouter);
