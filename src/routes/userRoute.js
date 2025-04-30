23f99e6ce0f7ed301b77caed44d9b2d7c695dfba
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2025-04-30 14:03:30 +0530

    Change userprofile route method from GET to POST for consistency in user actions

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -7,6 +7,6 @@ const userRouter = express.Router();
 
 userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
-userRouter.get("/userprofile", authenticate, rateLimiter, userProfile);
+userRouter.post("/userprofile", authenticate, rateLimiter, userProfile);
 
 export default userRouter;
